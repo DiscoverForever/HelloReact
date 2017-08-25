bd0f831816f470d600c4a7175f9e326515221016
tangyy <tangyueyang@lztogether.com>
2017-08-25 15:55:04 +0800

    Add NavBar

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
-import { hashHistory } from 'react-router';
-import  './NavBar.scss';
+import React, {Component} from 'react';
+import {hashHistory} from 'react-router';
+import './NavBar.scss';
 
 class NavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.handleTab = this.handleTab.bind(this);
-    }
-    /**
+  constructor(props) {
+    super(props);
+    this.handleTab = this
+      .handleTab
+      .bind(this);
+  }
+  /**
      * tab点击事件
-     * @param {Number} tabId 
+     * @param {Number} tabId
      */
-    handleTab(tabId) {
-        hashHistory.push({pathname:'login', state: {username:'1111', password: '2222'}});
-        console.log(tabId);
+  handleTab(tabId) {
+    switch (tabId) {
+      case 0:
+        hashHistory.push('/');
+        break;
+      case 1:
+        hashHistory.push({pathname: '/category'});
+        break;
+      case 2:
+        hashHistory.push('/shopcart');
+        break;
+      case 3:
+        hashHistory.push('/mine');
+        break;
     }
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li onClick={() => {this.handleTab(0)}}>主页</li>
-                    <li onClick={() => {this.handleTab(1)}}>分类</li>
-                    <li onClick={() => {this.handleTab(2)}}>购物车</li>
-                    <li onClick={() => {this.handleTab(3)}}>我的</li>
-                </ul>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          <li onClick={() => {
+            this.handleTab(0);
+          }}>主页</li>
+          <li onClick={() => {
+            this.handleTab(1);
+          }}>分类</li>
+          <li onClick={() => {
+            this.handleTab(2);
+          }}>购物车</li>
+          <li onClick={() => {
+            this.handleTab(3);
+          }}>我的</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
