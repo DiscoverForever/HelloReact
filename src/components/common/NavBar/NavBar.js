c8ecbe97b41e31a085c27cd787e88bad26f07bff
tangyy <tangyueyang@lztogether.com>
2017-08-29 09:14:53 +0800

    Mod beautify

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -3,52 +3,58 @@ import {hashHistory} from 'react-router';
 import './NavBar.scss';
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.handleTab = this
-      .handleTab
-      .bind(this);
-  }
-  /**
+    constructor(props) {
+        super(props);
+        this.handleTab = this
+            .handleTab
+            .bind(this);
+    }
+
+    /**
      * tab点击事件
      * @param {Number} tabId
      */
-  handleTab(tabId) {
-    switch (tabId) {
-      case 0:
-        hashHistory.push('/');
-        break;
-      case 1:
-        hashHistory.push({pathname: '/category'});
-        break;
-      case 2:
-        hashHistory.push('/shopcart');
-        break;
-      case 3:
-        hashHistory.push('/mine');
-        break;
+    handleTab(tabId) {
+        switch (tabId) {
+            case 0:
+                hashHistory.push('/');
+                break;
+            case 1:
+                hashHistory.push({pathname: '/category'});
+                break;
+            case 2:
+                hashHistory.push('/shopcart');
+                break;
+            case 3:
+                hashHistory.push('/mine');
+                break;
+        }
     }
-  }
-  render() {
-    return (
-      <div className="nav-wrapper">
-        <ul>
-          <li onClick={() => {
-            this.handleTab(0);
-          }}>主页</li>
-          <li onClick={() => {
-            this.handleTab(1);
-          }}>分类</li>
-          <li onClick={() => {
-            this.handleTab(2);
-          }}>购物车</li>
-          <li onClick={() => {
-            this.handleTab(3);
-          }}>我的</li>
-        </ul>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="nav-wrapper">
+                <ul>
+                    <li onClick={() => {
+                        this.handleTab(0);
+                    }}>主页
+                    </li>
+                    <li onClick={() => {
+                        this.handleTab(1);
+                    }}>分类
+                    </li>
+                    <li onClick={() => {
+                        this.handleTab(2);
+                    }}>购物车
+                    </li>
+                    <li onClick={() => {
+                        this.handleTab(3);
+                    }}>我的
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default NavBar;
